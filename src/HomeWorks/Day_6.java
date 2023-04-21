7649bb0eba590ad2c9687c72841a7c4860ea528b
azozrazoz <doshan.de@gmail.com>
2023-04-21 10:08:49 +0600

    Day_6

---
+++ src/HomeWorks/Day_6.java
@@ -0,0 +1,34 @@
+package HomeWorks;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day_6 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        try {
+            try {
+                int x = new Integer(sc.nextInt());
+                int y = new Integer(sc.nextInt());
+                try {
+                    System.out.println(x / y);
+                }
+                catch (ArithmeticException e) {
+                    System.out.println("java.lang.ArithmeticException: / by zero");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("java.util.InputMismatchException");
+            }
+
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
