1ddba275613e67c7fe1981e9f8b87430eda71616
azozrazoz <doshan.de@gmail.com>
2023-04-21 10:15:25 +0600

    Day_5

--- src/HomeWorks/Day_6.java
+++ src/HomeWorks/Day_6.java
@@ -11,6 +11,7 @@ public class Day_6 {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
+        if (Instance)
         try {
             try {
                 int x = new Integer(sc.nextInt());
